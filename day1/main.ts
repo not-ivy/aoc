78e38d27f4d33f54c9fe9f71286b488be6fe7de9
ivy <ivy@not-ivy.dev>
2022-12-02 10:43:52 -0800

    clarify day1

--- day1/main.ts
+++ day1/main.ts
@@ -2,10 +2,10 @@
 
 const input = Deno.readTextFileSync('day1/input.txt');
 
-const calories = input
+const maxCalorie = input
   .split('\n\n')
   .map((elf) => elf.split('\n').map((calorie) => parseInt(calorie, 10)))
   .map((elf) => elf.reduce((acc, calorie) => acc + calorie, 0))
   .reduce((acc, calorie) => Math.max(acc, calorie), 0);
 
-console.log(calories);
+console.log(maxCalorie);
