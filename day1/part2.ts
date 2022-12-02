94a147bae0170073eecb50e1aa1f89629eba7f31
ivy <ivy@not-ivy.dev>
2022-12-02 10:45:13 -0800

    add part 2

---
+++ day1/part2.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env deno run --allow-read
+
+const input = Deno.readTextFileSync('day1/input.txt');
+
+const calories = input
+  .split('\n\n')
+  .map((elf) => elf.split('\n').map((calorie) => parseInt(calorie, 10)))
+  .map((elf) => elf.reduce((acc, calorie) => acc + calorie, 0));
+
+const topThree = calories
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((acc, calorie) => acc + calorie, 0);
+
+console.log(topThree);
+
