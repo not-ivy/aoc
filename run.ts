e3c84116876ab089f30bc49a85aad33da451b0d7
ivy <ivy@not-ivy.dev>
2023-12-02 18:47:19 -0800

    load input remotely

--- run.ts
+++ run.ts
@@ -1,7 +1,18 @@
-const args = Deno.args;
-const [d, p] = args[0].match(/(d\d)|(p\d)/g)?.map((a) => a[1]) ?? [];
+import minimist from "https://esm.sh/minimist@1.2.8";
+import "https://deno.land/std@0.208.0/dotenv/load.ts";
+
+const args = minimist(Deno.args);
+
+const [d, p] = args._[0].match(/(d\d)|(p\d)/g)?.map((a) => a[1]) ?? [];
 const module = (await import(`./d${d}/p${p}/solve.ts`)).default;
-const file = Deno.readTextFileSync(
-  args[1] === "-p" ? `./d${d}/p${p}/prompt.txt` : `./d${d}/input.txt`,
-);
-console.log(module.solve(file));
+const input = args.r
+  ? (await (await fetch(`https://adventofcode.com/2023/day/${d}/input`, {
+    headers: {
+      "Cookie": `session=${Deno.env.get("session")}`,
+    },
+  })).text()).trim()
+  : (args.p
+    ? Deno.readTextFileSync(`./d${d}/p${p}/prompt.txt`)
+    : Deno.readTextFileSync(`./d${d}/input.txt`));
+
+console.log(module.solve(input));
